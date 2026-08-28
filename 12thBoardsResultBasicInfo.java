12df3a886fdd268ad2aa067a1cc19f5a39e60495
Rudra-Dev <rudrajena928@gmail.com>
2026-08-28 18:47:18 +0530

    Remove Students class and main method

--- 12thBoardsResultBasicInfo.java
+++ 12thBoardsResultBasicInfo.java
@@ -1,16 +1,1 @@
-class Students {
-    String name;
-    int age;
-    int boardsperc;
-    void display(String name,int age,int boardsperc) {
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("12th Boards Percentage: " + boardsperc);
-    }
-}
-public class Demo {
-    public static void main(String[] args) {
-        Students s = new Students();
-        s.display("Rudra", 17, 89);
-    }
-}
+
